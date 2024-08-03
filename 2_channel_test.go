4b1197e5ab34acf53db79bd7006a86cdf361434a
ricidkumbara <ricidkumbara@gmail.com>
2024-08-03 20:55:25 +0700

    update

--- 2_channel_test.go
+++ 2_channel_test.go
@@ -179,3 +179,18 @@ func TestDefaultSelect(t *testing.T) {
 		}
 	}
 }
+
+func TestRaceCondition(t *testing.T) {
+	x := 0
+
+	for i := 1; i < 1000; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				x = x + 1
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println(x)
+}
